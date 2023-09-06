58321c86fae47271dd723fcb0415c0c995a5cf07
jeffery <jeffery.wang@mobagel.com>
2023-09-06 21:51:29 +0800

    add comment

--- main.py
+++ main.py
@@ -11,19 +11,19 @@ def all_subsets(ss):
 
 
 def extract_links(text):
-    # 正则表达式模式匹配超链接
-    link_pattern = re.compile(r'\[.*?\]\((.*?)\)')  # 匹配 [text](url)
+    # extract the url
+    link_pattern = re.compile(r'\[.*?\]\((.*?)\)')  # extract [text](url)
     # link_pattern = re.compile(r'<a\s+[^>]*href="([^"]*)"[^>]*>')
     links = link_pattern.findall(text)
     return links
 
 def has_link_to_other_file(filepath_a, filepath_b):
     
-    # 读取文件A的内容
+    # read card A
     with open(filepath_a, 'r', encoding='utf-8') as f:
         content_a = f.read()
 
-    # 读取文件B的内容
+    # read card B
     with open(filepath_b, 'r', encoding='utf-8') as f:
         content_b = f.read()
         
@@ -35,18 +35,14 @@ def has_link_to_other_file(filepath_a, filepath_b):
     links_in_a = extract_links(content_a)
     links_in_b = extract_links(content_b)
 
-    # 检查链接中是否包含另一个文件的名称
+    # check if the content of A contains the title of B (and vice versa)
     for link in links_in_a:
         if re.findall(filename_b, link):
             return True
-        # else:
-        #     print(link, "False")
 
     for link in links_in_b:
         if re.findall(filename_a, link):
             return True
-        # else:
-        #     print(link, "False")
 
     return False
 
@@ -74,10 +70,10 @@ def generate_nx_graph(target_dir):
 
 def extract_zip_to_temp_folder(zip_path):
     try:
-        # 創建一個暫存資料夾
+        # create temp folder
         temp_folder = tempfile.mkdtemp()
 
-        # 解壓縮zip檔案到暫存資料夾
+        # unzip files to the temp folder
         with zipfile.ZipFile(zip_path, 'r') as zip_ref:
             zip_ref.extractall(temp_folder)
 
@@ -88,29 +84,8 @@ def extract_zip_to_temp_folder(zip_path):
     
 def main(target_dir, filename):
 
-    import matplotlib.pyplot as plt
-    # font_path = '/Users/jefferywang/Desktop/Things/src/source-han-serif-1.001R/OTC/SourceHanSerif-Medium.ttc'  # 將路徑替換為實際的字體文件路徑
-
-    # 設定中文字體
-    # from matplotlib.font_manager import fontManager
-    # fontManager.addfont(font_path)
-    plt.figure(figsize=(16,10))
-
-    
-    # zip_file_path = f"{filepath}/{filename}"
-    # target_dir = extract_zip_to_temp_folder(zip_file_path)
     g = generate_nx_graph(target_dir)
     
-    # plot networkx
-    # random_pos = nx.random_layout(g, seed=42)
-    # pos = nx.spring_layout(g, pos=random_pos)  # 選擇一個佈局方式
-    # nx.draw_networkx(g, pos, with_labels=False, node_size=100, node_color='skyblue', edge_color="black")
-    # nx.draw_networkx_labels(g, pos=pos, verticalalignment="top", horizontalalignment="center")
-    
-    # output_png_path = f"{target_dir}/{filename.replace('.zip', '')}.png"
-    # plt.savefig(output_png_path, format="png")
-    # return output_png_path
-
     # plot pyvis network
     pyvis_graph = net.Network(height='800px', width='100%',heading=filename)
     pyvis_graph.from_nx(g)
@@ -121,6 +96,4 @@ def main(target_dir, filename):
 
 
 if __name__ == "__main__":
-    # filename = "生存的法則一.zip"
-    # filepath = '/Users/jefferywang/Desktop/Things/Practice/md-graph'
     main()
